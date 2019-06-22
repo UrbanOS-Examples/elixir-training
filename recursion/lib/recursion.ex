ba38101a82b767231208e8fb46af73f57d9f5ef6
jeffgrunewald <jeff@grunewalddesign.com>
2019-06-22 10:31:10 -0400

    Implementation of count

--- recursion/lib/recursion.ex
+++ recursion/lib/recursion.ex
@@ -12,4 +12,25 @@ defmodule Recursion do
   def reduce([head | tail], acc, func) do
     reduce(tail, func.(head, acc), func)
   end
+
+  @doc """
+  Counts the number of elements in a collection.
+  Uses a 'named' anonymous function bound to
+  a variable and called with the '.' operator and
+  capture syntax.
+  """
+  def count(enum) do
+    counter = fn _num, acc -> 1 + acc end
+
+    reduce(enum, 0, &counter.(&1, &2))
+  end
+
+  @doc """
+  Alternative count; counts the elements in a
+  collection with a direct call to an anonymous
+  function.
+  """
+  def count_anon(enum) do
+    reduce(enum, 0, fn _num, acc -> 1 + acc end)
+  end
 end
